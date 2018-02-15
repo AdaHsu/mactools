57a194f6f5f6764ec4d62373ed7f62c3ce69475f
Ada Hsu <ada@adahsu.tw>
2018-02-15 12:05:31 +0800

    Add check_cask.sh

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # mactools
+
+Some utilities for me to administrate my macOS.
+
+用於管理 macOS 時所使用的一些小東西。
+
+## check_cask.sh
+
+Check what I had installed version via **brew-cask** and notice you when new version comming.
+
+If you have don't need notice some software's version, add it in **EXCLUDE_SOFTWARE** variable.
+
+用於檢查透過 **brew-cask** 所安裝的軟體，並在版本更新時予以提示。
+
+若有任何不需列入版本更新通知的軟體，請將其加入 **EXCLUDE_SOFTWARE** 變數內。
---
+++ check_cask.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#######################################################
+#
+# check_cask.sh Check software installed via brew-cask.
+# Copyright (C) 2018 Ada Hsu, Taiwan.
+#
+#  This file is free software: you may copy, redistribute and/or modify it   
+#  under the terms of the Apache License, Version 2.0.
+#  You may obtain a copy of the License at  
+#
+#  https://www.apache.org/licenses/LICENSE-2.0  
+#
+#######################################################
+
+# softwares without checking it's version.
+EXCLUDE_SOFTWARE="filezilla firefox google-chrome intellij-idea istat-menus iterm2 sourcetree visual-studio-code "
+
+# check shell, only zsh and bash
+SH=$(ps -o comm= -p $$)
+
+if [ "${SH}" = "zsh" ]
+then
+    OFFSET0=1
+    OFFSET1=2
+else
+    OFFSET0=0
+    OFFSET1=1
+fi
+
+IFS='
+'
+for SOFTWARE in $(brew cask list)
+do
+    BOTTLE=$(brew cask info ${SOFTWARE} | grep -A 1 '==> Name' | tail -n 1)
+    VERSION_INFO=($(brew cask info ${SOFTWARE} | sed -n -e 1p -e 3p))
+    CURRENT_VERSION=$(echo ${VERSION_INFO[${OFFSET0}]} | cut -d\  -f 2)
+    INSTALLED_VERSION=$(echo ${VERSION_INFO[${OFFSET1}]} | cut -d\  -f 1 | awk -F/ '{ print $NF }')
+    NAME=$(echo ${VERSION_INFO[${OFFSET0}]} | cut -d: -f 1)
+
+    echo -n ${BOTTLE} \[${NAME}\]
+    if [ "${EXCLUDE_SOFTWARE/${SOFTWARE}/}" = "${EXCLUDE_SOFTWARE}" ]
+    then
+        if [ "${CURRENT_VERSION}" != "${INSTALLED_VERSION}" ]
+        then
+            echo -n ": ${INSTALLED_VERSION} installed, but new update ${CURRENT_VERSION} exists."
+        fi    
+    fi
+    echo
+done
