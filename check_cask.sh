50f57a4fb0819e407a90470489a6562ad87e66a3
Ada Hsu <ada@adahsu.net>
2019-10-04 09:17:32 +0800

    新增排除套件 araxis-merge

--- check_cask.sh
+++ check_cask.sh
@@ -14,7 +14,7 @@
 #######################################################
 
 # softwares without checking it's version.
-EXCLUDE_SOFTWARE="firefox google-chrome istat-menus iterm2 sourcetree visual-studio-code telegram gitkraken adguard iina"
+EXCLUDE_SOFTWARE="firefox google-chrome istat-menus iterm2 sourcetree visual-studio-code telegram gitkraken adguard iina araxis-merge"
 
 COLOR_RESET='\033[0m'
 COLOR_HIGHLIGHT='\033[1;37m'
