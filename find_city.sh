472837199a539b831dd681dde4f77cae6ecf9778
Ada Hsu <ada@adahsu.net>
2020-01-08 09:26:22 +0800

    修正擷取規則

--- find_city.sh
+++ find_city.sh
@@ -2,4 +2,4 @@
 
 CITY_INFO_FILE=/tmp/city
 
-curl -s https://freegeoip.app/csv/ | cut -d, -f 6 > ${CITY_INFO_FILE}
+curl -s https://freegeoip.app/csv/ | cut -d, -f 5 | cut -d' ' -f 1 > ${CITY_INFO_FILE}
--- get_weather.sh
+++ get_weather.sh
@@ -5,8 +5,9 @@ WEATHER_INFO_FILE=/tmp/weather
 CITY_INFO_FILE=/tmp/city
 CITY_NAME=$(head -n 1 ${CITY_INFO_FILE})
 
-WEATHER_RAW=$(curl -s "https://wttr.in/${CITY_NAME}?format=3")
-WEATHER_CITY=$(echo ${WEATHER_RAW} | cut -d: -f 1 | cut -d, -f 1 | cut -d\  -f 1)
-WEATHER_STATUS=$(echo ${WEATHER_RAW} | cut -d: -f 2)
+WEATHER_RAW=$(curl -s "https://wttr.in/${CITY_NAME}?format=%C,%t")
+WEATHER_STATUS=$(echo ${WEATHER_RAW} | cut -d, -f 1)
+WEATHER_TEMPERATURE=$(echo ${WEATHER_RAW} | cut -d, -f 2 | tr -d "\n" | tr -d "\r")
+# WEATHER_STATUS_LOCAL=$(trans "${WEATHER_STATUS}" -from en-US -to zh-TW -show-original-phonetics N -show-original N | head -n 1 | tr -d "\n" | tr -d "\r")
 # echo "${WEATHER_CITY}:${WEATHER_STATUS}" > ${WEATHER_INFO_FILE}
-echo "${WEATHER_STATUS}" > ${WEATHER_INFO_FILE}
+echo "${WEATHER_STATUS}, ${WEATHER_TEMPERATURE}" > ${WEATHER_INFO_FILE}
